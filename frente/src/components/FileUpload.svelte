065782ad543b12b68c93425163eba0d6b13003d8
Pepemi <josemiguelppmi@gmail.com>
2025-05-16 08:58:28 +0000

    prueba2 cambio ruta icono

--- frente/src/components/FileUpload.svelte
+++ frente/src/components/FileUpload.svelte
@@ -74,7 +74,7 @@
   <svg aria-hidden="true" class="w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
   {#if nomFile === ''}
     <p class="text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click para subir el archivo .gpx</span> o arrastra y suéltalo</p>
-    <p class="text-xs text-gray-500 dark:text-gray-400">Sólo admitidos archivos GPX (MAX. ?? Mb)</p>
+    <p class="text-xs text-gray-500 dark:text-gray-400">Sólo admitidos archivos GPX (MAX. 5 Mb)</p>
   {:else}
     <p>Archivo seleccionado: {nomFile}</p>
   {/if}
