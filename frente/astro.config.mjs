fab0a5c435964d60a3fc97f637def31a9f420ed5
Pepemi <josemiguelppmi@gmail.com>
2025-05-16 08:44:52 +0000

    logo añadido y imagenes deberian funcionar en gh pages

--- frente/astro.config.mjs
+++ frente/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ppmico.github.io',
+  base: '/traveRevisor',
   integrations: [
     tailwind(),
     svelte()
